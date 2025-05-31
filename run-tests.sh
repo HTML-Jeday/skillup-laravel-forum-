6a4609affba0d05d2bf474539a6ad7117de9617b
serhii <sergeykobzarr33@gmail.com>
2025-05-31 16:45:38 +0300

    test

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Exit immediately on error
+set -e
+
+# Print each command before executing (for debugging)
+set -x
+
+# Detect CI environment
 if [ -n "$GITHUB_ACTIONS" ]; then
   export DB_HOST=127.0.0.1
   export DB_DATABASE=laravel_testing
@@ -12,5 +19,5 @@ else
   export DB_PASSWORD=
 fi
 
-php artisan test --env=testing "$@"
-exit $?
+# Run Pest directly instead of Laravel's wrapper
+exec ./vendor/bin/pest --stop-on-failure "$@"
