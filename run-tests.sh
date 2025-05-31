6a63f6a5f65740ff9ce96e0b05648be084c22100
serhii <sergeykobzarr33@gmail.com>
2025-05-31 16:43:10 +0300

    test

--- run-tests.sh
+++ run-tests.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Check if we're running in a CI environment (GitHub Actions)
 if [ -n "$GITHUB_ACTIONS" ]; then
-  # Use CI environment settings
   export DB_HOST=127.0.0.1
   export DB_DATABASE=laravel_testing
   export DB_USERNAME=root
   export DB_PASSWORD=root
 else
-  # Use local Docker environment settings
   export DB_HOST=db
   export DB_DATABASE=skillup
   export DB_USERNAME=root
   export DB_PASSWORD=
 fi
 
-# Run the tests with environment variables overriding phpunit.xml settings
 php artisan test --env=testing "$@"
+exit $?
